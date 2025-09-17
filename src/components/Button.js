e8358ec183db2d2b40bd2caff39152aca169fe08
Riham274 <riham.ararawi274@gmail.com>
2025-09-17 22:37:44 +0300

    Add Compose component

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+ function Button (props) {
+    return(
+        <button className="react-btn">
+            {props.title}
+        </button>
+    
+    )
+ }
+
+ export default Button;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,12 @@
+import Button from "./Button";
+
 function Product (props) {
     return (
         <div className="product-card">
             <h1>{props.title}</h1>
             <p>{props.description}</p>
             <span>{props.price}</span>
+            <Button title="details"/>
         </div>
     )
 }
