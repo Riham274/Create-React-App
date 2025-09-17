acd264e3d9141266fcde5b871746520abb77c4d9
Riham274 <riham.ararawi274@gmail.com>
2025-09-17 23:49:41 +0300

    Create useState lifecycle demo

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,18 +3,35 @@ import React from "react";
 class Welcome extends React.Component{
   state = {
     fname: "riham",
-    lname: "ahmad"
+    lname: "ahmad" 
   }
+
     render(){
+      console.log("render phase")
       console.log(this.state);
+      console.log("state phase")
+
         return (
         <>
           <h1>hello, {this.props.name}</h1>
           <p>age: {this.props.age}</p>
           <p>title: {this.props.title}</p>
+          <button onClick={() => {
+            this.setState({
+              lname: "soso"
+             })
+          }
+          }>
+            change my name
+          </button>
           <h6>Created by, {this.state.fname} {this.state.lname}</h6>
         </>
         );
+    }
+    componentDidMount(){
+      
+      console.log("component phase")
+
     }
 
 }
