7544346499a33bd2e9df1d382d2cb538d3fef79d
Riham274 <riham.ararawi274@gmail.com>
2025-09-17 23:10:57 +0300

    Adding local state

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ function App(){
     {/*
      <WelcomeFC name="ahmad" age="20" title="js dev"/>
      <WelcomeFC name="riham" age="18" title="ts dev"/>
-     <Welcome name="yara" age="15" title="ts dev"/>*/}
+     <Welcome name="yara" age="15" title="ts dev"/>
      <Product title="product-1" price="100" description="lorem lorem lorem 1"/>
      <Product title="product-2" price="200" description="lorem lorem lorem 2"/>
      <Product title="product-3" price="300" description="lorem lorem lorem 3"/>
+     */}
+     <Welcome name="yara" age="15" title="ts dev"/>
      
     </div>
   );
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 class Welcome extends React.Component{
+  state = {
+    fname: "riham",
+    lname: "ahmad"
+  }
     render(){
-      console.log(this.props);
+      console.log(this.state);
         return (
         <>
           <h1>hello, {this.props.name}</h1>
           <p>age: {this.props.age}</p>
           <p>title: {this.props.title}</p>
+          <h6>Created by, {this.state.fname} {this.state.lname}</h6>
         </>
         );
     }
