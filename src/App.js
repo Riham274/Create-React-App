18457c7856d7fff74739d0572d00da71d7a17fda
Riham274 <riham.ararawi274@gmail.com>
2025-09-18 23:56:17 +0300

    Clean code

--- src/App.js
+++ src/App.js
@@ -8,19 +8,6 @@ import WelcomeFC from "./components/WelcomeFC";
 function App(){
   return(
     <div className="App">
-    {/*
-     <WelcomeFC name="ahmad" age="20" title="js dev"/>
-     <WelcomeFC name="riham" age="18" title="ts dev"/>
-     <Welcome name="yara" age="15" title="ts dev"/>
-     <Product title="product-1" price="100" description="lorem lorem lorem 1"/>
-     <Product title="product-2" price="200" description="lorem lorem lorem 2"/>
-     <Product title="product-3" price="300" description="lorem lorem lorem 3"/>
-    
-     <Welcome name="yara" age="15" title="ts dev"/>
-     <WelcomeFC name="ahmad" age="20" title="js dev"/>
-     <WelcomeFC name="riham" age="18" title="ts dev"/>
-     <Button title="hiii"/>
-     */}
      <Input label="name" type="text"/>
      <Input label="password" type="password"/>
      
