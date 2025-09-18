8542e6c55c5c4a63d9f631c3fc2eec311841ca0d
Riham274 <riham.ararawi274@gmail.com>
2025-09-19 01:21:14 +0300

    Refactor Form component to class component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,73 @@
 import { useState } from "react";
+import React from "react";
 
 
-function Form () {
+class Form extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            password: '',
+            gender: ''
+        }
+    }
+    nameHandler = (value) => {
+        this.setState({name: value});
+
+    }
+
+    passwordHandler = (value) => {
+        this.setState({password: value});
+
+    }
+
+    genderHandler = (value) => {
+        this.setState({gender: value});
+
+    }
+    render() {
+        return(
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            console.log("form submited");
+            console.log(this.state);
+        }}>
+            <label>Name</label>
+            <input 
+            type="text" 
+            placeholder="your name" 
+            onChange={(event) => {
+                this.nameHandler(event.target.value)
+            }}/>
+            <br/>
+
+            <label>Password</label>
+            <input 
+            type="password" 
+            placeholder="your password" 
+            onChange={(event) => {
+                this.passwordHandler(event.target.value)
+            }}/>
+
+            <br/>
+            <label>Gender</label>
+            <select
+             onChange={(event) => {
+                this.genderHandler(event.target.value)
+            }}
+            >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
+            <button type="submit">Submit</button>
+
+        </form>
+    );
+    }
+
+
+}
+/* function Form () {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const [gender, setGender] = useState('');
@@ -61,5 +127,6 @@ function Form () {
         </form>
     );
 }
+*/
 
 export default Form;
